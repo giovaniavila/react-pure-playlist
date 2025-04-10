56403a1fe76ef49ff6f34863d4bfd726b59e1a8e
giovani <giovanicarvalhoavila@hotmail.com>
2025-04-10 17:54:02 -0300

    chore: add initial config for react, astro and tailwind

---
+++ src/components/Playlist.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Playlist = () => {
+  return (
+    <div className='bg-amber-400'>
+      working with react and astro.js + tailwind
+    </div>
+  )
+}
+
+
